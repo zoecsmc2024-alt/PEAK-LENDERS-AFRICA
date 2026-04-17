3e852dc537bb338bda60267a07bd1fbce28c64af
zoecsmc2024-alt <zoecsmc2024@gmail.com>
2026-04-17 18:40:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2734,7 +2734,7 @@ def show_payroll():
     # ==============================
     # 📑 TABS
     # ==============================
-    tab_process, tab_logs = st.tabs(["💳 Process Payroll", "📜 Payroll Ledger"])
+    tab_process, tab_logs, tab2 = st.tabs(["💳 Process Payroll", "📜 Payroll Ledger"])
 
     # ==============================
     # 💳 PROCESS PAYROLL
