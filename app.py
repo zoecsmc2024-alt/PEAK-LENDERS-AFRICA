9a1741135740acd16046261fd80af57ac15eb464
zoecsmc2024-alt <zoecsmc2024@gmail.com>
2026-05-04 16:18:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from modules import calendar
 from modules import ledger
 from modules import payroll
 from modules import expenses
-from modules import Pettycash
+from modules import Petty Cash
 from modules import payments
 from modules import reports
 from modules import settings
