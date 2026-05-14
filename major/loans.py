c01f08fb7e61d17c024999a949ad3903e77073dc
zoecsmc2024-alt <zoecsmc2024@gmail.com>
2026-05-14 12:04:10 +0300

    Update loans.py

--- major/loans.py
+++ major/loans.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-
+from datetime import datetime, timedelta
 # Core DB utilities
 from core.database import supabase, get_cached_data, save_data_saas, delete_data_saas
 
